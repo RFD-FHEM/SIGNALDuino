7fca936211d4119d7c269755d603d69ee926470d
sidey79 <s.butzek@gmx.de>
2016-03-04 23:02:42 +0100

    Updated Firmware to 3.2.0-hf1

--- __vm/.SIGNALDuino.vsarduino.h
+++ __vm/.SIGNALDuino.vsarduino.h
@@ -5,15 +5,16 @@
 	        all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
 	        note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
-	Hardware: Arduino Mega ADK, Platform=avr, Package=arduino
+	Hardware: Arduino Nano w/ ATmega328, Platform=avr, Package=arduino
 */
 
 #ifndef _VSARDUINO_H_
 #define _VSARDUINO_H_
-#define __AVR_ATmega2560__
+#define __AVR_ATmega328p__
+#define __AVR_ATmega328P__
 #define F_CPU 16000000L
 #define ARDUINO 10607
-#define ARDUINO_AVR_ADK
+#define ARDUINO_AVR_NANO
 #define ARDUINO_ARCH_AVR
 #define __cplusplus 201103L
 #define __AVR__
