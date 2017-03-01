8216f67ff6639542e571fe37cfc5f7f4dee686d8
Ralf9 <ralfk_forum@gmx.de>
2017-03-01 23:10:13 +0100

    Update signalDecoder.cpp
    message compress bei der Ausgabe vom MS Nachrichten zugefügt

--- src/_micro-api/libraries/signalDecoder/src/signalDecoder.cpp
+++ src/_micro-api/libraries/signalDecoder/src/signalDecoder.cpp
@@ -89,13 +89,16 @@ inline void SignalDetectorClass::doDetect()
 
 //		if (messageLen == 0) pattern_pos = patternLen = 0;
 		//if (messageLen == 0) valid = true;
-	if (!valid)
-	{
-		// Try output
-
-		m_overflow = (messageLen == maxMsgSize) ? true : false;
-		processMessage();
-
+	if (!valid) {
+		if (mcDetected == true || messageLen >= minMessageLen) {
+			// Try output
+			m_overflow = (messageLen == maxMsgSize) ? true : false;
+			processMessage();
+		} else {
+			MsMoveCount = 3;
+			reset();
+		}
+		
 	}
 	else if (messageLen == minMessageLen) {
 		state = detecting;  // Set state to detecting, because we have more than minMessageLen data gathered, so this is no noise
@@ -289,33 +292,99 @@ void SignalDetectorClass::processMessage()
 				//postamble = "";
 
 				/*				Output raw message Data				*/
-				MSG_PRINT(MSG_START); 				
-				MSG_PRINT("MS"); MSG_PRINT(SERIAL_DELIMITER);
-				for (uint8_t idx = 0; idx < patternLen; idx++)
-				{
-					if (pattern[idx] == 0 || histo[idx] == 0) continue;
-					MSG_PRINT('P'); MSG_PRINT(idx); MSG_PRINT('='); MSG_PRINT(pattern[idx]); MSG_PRINT(SERIAL_DELIMITER);
-				}
-				MSG_PRINT('D=');
+				if (MredEnabled) {
+					int patternInt;
+					uint8_t patternLow;
+					uint8_t patternIdx;
+					
+					MSG_PRINT(MSG_START);  MSG_PRINT("Ms");  MSG_PRINT(SERIAL_DELIMITER);
+					for (uint8_t idx = 0; idx < patternLen; idx++)
+					{
+						if (pattern[idx] == 0 || histo[idx] == 0) continue;
+						patternIdx = idx;
+						patternInt = pattern[idx];
 
-				for (uint8_t i = mstart; i <= mend; ++i)
-				{
-					MSG_PRINT(message[i]);
-				}
-				MSG_PRINT(SERIAL_DELIMITER);
-				MSG_PRINT("CP="); MSG_PRINT(clock);     MSG_PRINT(SERIAL_DELIMITER);     // ClockPulse
-				MSG_PRINT("SP="); MSG_PRINT(sync);      MSG_PRINT(SERIAL_DELIMITER);     // SyncPulse
-				MSG_PRINT("R=");  MSG_PRINT(rssiValue); MSG_PRINT(SERIAL_DELIMITER);     // Signal Level (RSSI)
+						if (patternInt < 0) {
+							patternIdx = idx | B10100000;    // Bit5 = 1 (Vorzeichen negativ)
+							patternInt = -patternInt;
+						}
+						else {
+							patternIdx = idx | B10000000;    // Bit5 = 0 (Vorzeichen positiv)
+						}
+						
+						patternLow = lowByte(patternInt);
+						if (bitRead(patternLow,7) == 0) {
+							bitSet(patternLow,7);
+						}
+						else {
+							bitSet(patternIdx, 4);   // wenn bei patternLow Bit7 gesetzt ist, dann bei patternIdx Bit4 = 1
+						}
+						Serial.write(patternIdx);
+						Serial.write(patternLow);
+						Serial.write(highByte(patternInt) | B10000000);
+						MSG_PRINT(SERIAL_DELIMITER);
+					}
 
-				if (m_overflow) {
-					MSG_PRINT("O");
+					uint8_t n;
+
+					if ((mend & 1) == 1) {   // ungerade
+						MSG_PRINT("D");
+					}
+					else {
+						MSG_PRINT("d");
+					}
+					if ((mstart & 1) == 1) {  // ungerade
+						mstart--;
+						n = (message.getByte(mstart/2) & 15) | 128;    // high nibble = 8 als Kennzeichen für ungeraden mstart
+						Serial.write(n);
+						//MSG_WRITE(n);
+ 						mstart += 2;
+					}
+					for (uint8_t i = mstart; i <= mend; i=i+2) {					
+						n = message.getByte(i/2);
+						Serial.write(n);
+						//MSG_WRITE(n);
+					}
+
 					MSG_PRINT(SERIAL_DELIMITER);
-				}
-				MSG_PRINTLN(MSG_END);
-				//postamble.concat('\n');
-				//MSG_PRINT(postamble);
+					MSG_PRINT("C");  MSG_PRINT(clock, HEX);  MSG_PRINT(SERIAL_DELIMITER);
+					MSG_PRINT("S");  MSG_PRINT(sync, HEX);  MSG_PRINT(SERIAL_DELIMITER);
+					MSG_PRINT("R");  MSG_PRINT(rssiValue, HEX);  MSG_PRINT(SERIAL_DELIMITER);				}
+				else {
+					MSG_PRINT(MSG_START); MSG_PRINT("MS");  MSG_PRINT(SERIAL_DELIMITER);		
+					for (uint8_t idx = 0; idx < patternLen; idx++)
+					{
+						if (pattern[idx] == 0 || histo[idx] == 0) continue;
+						MSG_PRINT('P'); MSG_PRINT(idx); MSG_PRINT('='); MSG_PRINT(pattern[idx]); MSG_PRINT(SERIAL_DELIMITER);
+					}
+					MSG_PRINT("D=");
 
-				//printMsgRaw(mstart, mend, &preamble, &postamble);
+					for (uint8_t i = mstart; i <= mend; ++i)
+					{
+						MSG_PRINT(message[i]);
+					}
+					MSG_PRINT(SERIAL_DELIMITER);
+					MSG_PRINT("CP="); MSG_PRINT(clock);     MSG_PRINT(SERIAL_DELIMITER);     // ClockPulse
+					MSG_PRINT("SP="); MSG_PRINT(sync);      MSG_PRINT(SERIAL_DELIMITER);     // SyncPulse
+					MSG_PRINT("R=");  MSG_PRINT(rssiValue); MSG_PRINT(SERIAL_DELIMITER);     // Signal Level (RSSI)					
+				}
+				
+				if (m_overflow) {
+					MSG_PRINT("O");  MSG_PRINT(SERIAL_DELIMITER);
+				}
+				
+				if ((messageLen - mend) >= minMessageLen && MsMoveCount > 0) {
+					//MSG_PRINT(F("MS move. messageLen ")); MSG_PRINT(messageLen); MSG_PRINT(" "); MSG_PRINTLN(MsMoveCount)
+					MsMoveCount--;
+					bufferMove(mend+1);
+					//MSG_PRINT(F("MS move. messageLen ")); MSG_PRINTLN(messageLen);
+					mstart = 0;
+					MSG_PRINT("m"); MSG_PRINT(MsMoveCount) MSG_PRINT(SERIAL_DELIMITER);
+				}
+				
+				MSG_PRINT(MSG_END);
+				MSG_PRINT("\n");
+				
 				success = true;
 				m_truncated = false;
 #ifdef mp_crc
