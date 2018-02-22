b01389d5cc93a7bfeef41da4235c8a243c1f9186
sidey79 <7968127+sidey79@users.noreply.github.com>
2018-02-22 22:53:33 +0100

    Addeed Unit Test suite using googletest and win32arduino
    Tests MCbasicA1, MCbasicA2, MCbasicB, MCbasicC ,MCbasicD, MCbasicNA1, MCbasicNA2, MCHideki2, MCOSV11 are okay
    
    Added googleest submodule
    
    testSignalDecoder
    
    Tests und Decoder für OSV2 korrigiert. Polairät wird gleichbleibend erkannt
    
    tests/testSignalDecoder/tests.cpp
    
    Tests für mcHideki2, mcHideki3, mcHideki4, mcHideki5, mcHideki6, mcHideki7 angepasst da das Ergebnis in Ordnung ist
    
    testSignalDecoder/tests.cpp
    
    mcMaverick1 wird invertiert erkannt.
    
    signalDecoder.cpp   tests.cpp
    
    Anpassung mcMaverick1 ( TODO: Invertierung im FHEM Modul notwendig)
    
    signalDecoder.cpp
    
    Code in ManchesterpatternDecoder::doDecode() aufgeräumt
    
    signalDecoder.cpp
    
    DMc und Mc Ausgabe auf DMC und MC umgestellt, da Daten nicht mehr invertiert sind
    
    signalDecoder/src/signalDecoder.cpp
    
    ManchesterpatternDecoder::doDecode() umgeschrieben um Manchester Signal exakter demodulieren zu können  #75

---
+++ tests/testSignalDecoder/tests.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <gtest/gtest.h>
+#include "signalDecoder.h"
+
+namespace arduino { namespace test
+{
+  int duration;
+
+  class Tests : public ::testing::Test
+  {
+  public:
+	Tests() : mcdecoder(&ooDecode) { } ;
+
+	SignalDetectorClass ooDecode;
+	ManchesterpatternDecoder mcdecoder;
+
+	#define MSG_START char(0x2)		// this is a non printable Char
+	#define MSG_END   char(0x3)			// this is a non printable Char
+
+	bool state;
+
+	bool DigitalSimulate(const int pulse);
+	bool import_sigdata(std::string *cmdstring, const bool raw = false);
+	bool import_mcdata(std::string *cmdstring, const uint8_t startpos, const uint8_t endpos, const int16_t clock);
+	std::string Tests::geFullMCString();
+
+
+    virtual void SetUp();
+    virtual void TearDown();
+
+	static void SetUpTestCase() {
+	
+	};
+  };
+
+} // End namespace test
+} // End namespace arduino
