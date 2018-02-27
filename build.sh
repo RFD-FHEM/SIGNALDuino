78c23b71f94546857bb041e5a7217a54f450fe38
sidey79 <7968127+sidey79@users.noreply.github.com>
2018-02-27 23:25:42 +0100

    travis

--- build.sh
+++ build.sh
@@ -6,7 +6,14 @@ fi
 
 if [ "${BOARD}" = "nano" ]; then
   arduino --board arduino:avr:nano --save-prefs
+elif [ "${BOARD}" = "minicul" ]; then
+  echo "compiler.cpp.extra_flags=-DARDUINO_ATMEGA328P_MINICUL=1" > /usr/local/share/arduino/hardware/arduino/avr/platform.local.txt
+  arduino --board arduino:avr:promini --save-prefs
+elif [ "${BOARD}" = "RADINO" ]; then
+  echo "compiler.cpp.extra_flags=-DARDUINO_AVR_ICT_BOARDS_ICT_BOARDS_AVR_RADINOCC1101=1" > /usr/local/share/arduino/hardware/arduino/avr/platform.local.txt
+  arduino --board In-Circuit:radino:radino CC1101 --save-prefs
 fi
+
 
 echo "Compile now for ${BOARD} with ${RECEIVER}"
 arduino -v --verbose-build --verify $PWD/RF_Receiver/RF_Receiver.ino 2>&1
