c6a88517894951dad20ae90e8536e90669699404
sidey79 <7968127+sidey79@users.noreply.github.com>
2018-02-28 00:12:43 +0100

    travis

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ if [ "${BOARD}" = "nano" ]; then
 elif [ "${BOARD}" = "minicul" ]; then
   echo "compiler.cpp.extra_flags=-DARDUINO_ATMEGA328P_MINICUL=1" > /usr/local/share/arduino/hardware/arduino/avr/platform.local.txt
   arduino --board arduino:avr:promini --save-prefs 2>&1
-elif [ "${BOARD}" = "RADINO" ]; then
+elif [ "${BOARD}" = "radino" ]; then
   echo "compiler.cpp.extra_flags=-DARDUINO_AVR_ICT_BOARDS_ICT_BOARDS_AVR_RADINOCC1101=1" > /usr/local/share/arduino/hardware/arduino/avr/platform.local.txt
  arduino --install-boards "In-Circuit:avr"  2>&1
  arduino --board In-Circuit:avr:radinoCC1101 --save-prefs 2>&1
