5cecf45862f6d20d328e4ae914d5558ecbdfae90
sidey79 <7968127+sidey79@users.noreply.github.com>
2018-03-01 23:39:16 +0100

    travis

--- build.sh
+++ build.sh
@@ -5,12 +5,12 @@ if [ "${RECEIVER}" = "cc1101" ]; then
 fi
 
 if [ "${BOARD}" = "nano" ]; then
-  arduino --board arduino:avr:nano --save-prefs 2>&1
+  arduino --board arduino:avr:nano:cpu=atmega328 --save-prefs 2>&1
 elif [ "${BOARD}" = "minicul" ]; then
   echo "compiler.cpp.extra_flags=-DARDUINO_ATMEGA328P_MINICUL=1" > /usr/local/share/arduino/hardware/arduino/avr/platform.local.txt
-  arduino --board arduino:avr:pro --save-prefs 2>&1
+  arduino --BOARD arduino:avr:pro:cpu=8MHzatmega328 --save-prefs 2>&1
 elif [ "${BOARD}" = "promini" ]; then
-  arduino --board arduino:avr:pro --save-prefs 2>&1
+  arduino --BOARD arduino:avr:pro:cpu=8MHzatmega328 --save-prefs 2>&1
 elif [ "${BOARD}" = "radino" ]; then
   echo "compiler.cpp.extra_flags=-DARDUINO_AVR_ICT_BOARDS_ICT_BOARDS_AVR_RADINOCC1101=1" > /usr/local/share/arduino/hardware/arduino/avr/platform.local.txt
  arduino --install-boards "In-Circuit:avr"  2>&1
