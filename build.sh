9f0099bc4d3a92d8deb859f1d10385ef52bc4c4c
sidey79 <7968127+sidey79@users.noreply.github.com>
2018-03-05 22:46:15 +0100

    travis deploy enhancement

--- build.sh
+++ build.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
-set -ev
+#set -e
+while test $# -gt 0
+do
+    case "$1" in
+        BOARD*)   
+               BOARD=`echo $1 | cut -d= -f2`
+            ;;
+        RECEIVER*) 
+               RECEIVER=`echo $1 | cut -d= -f2`
+		;;
+        *) echo "got argument $1"
+            ;;
+    esac
+    shift
+done
+
+
 if [ "${RECEIVER}" = "cc1101" ]; then
   echo "compiler.cpp.extra_flags=-DOTHER_BOARD_WITH_CC1101=1" > /usr/local/share/arduino/hardware/arduino/avr/platform.local.txt
 fi
 
+
 if [ "${BOARD}" = "nano" ]; then
   arduino --board arduino:avr:nano:cpu=atmega328 --save-prefs 2>&1
+
 elif [ "${BOARD}" = "minicul" ]; then
   echo "compiler.cpp.extra_flags=-DARDUINO_ATMEGA328P_MINICUL=1" > /usr/local/share/arduino/hardware/arduino/avr/platform.local.txt
   arduino --board arduino:avr:pro:cpu=8MHzatmega328 --save-prefs 2>&1
@@ -19,4 +37,4 @@ fi
 
 
 echo "Compile now for ${BOARD} with ${RECEIVER}"
-arduino -v --verbose-build --verify $PWD/RF_Receiver/RF_Receiver.ino 2>&1
+#arduino -v --verbose-build --verify $PWD/RF_Receiver/RF_Receiver.ino 2>&1
