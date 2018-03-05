98580dec7b72485890986b9367368eaad620b13c
sidey79 <7968127+sidey79@users.noreply.github.com>
2018-03-05 22:46:15 +0100

    build.sh
    added option to build with script arguments (currently not used)
    
    travis.yml
    travis will deploy servial compiled binarys if a tag is set
    
    deploy.sh
    added an script to deploy to guthub releases, but it is currently not working

--- build.sh
+++ build.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
-set -ev
+#set -e
+while test $# -gt 0
+do
+    case "$1" in
+        BOARD*)   
+               export BOARD=`echo $1 | cut -d= -f2`
+        ;;
+        RECEIVER*) 
+               export RECEIVER=`echo $1 | cut -d= -f2`
+		;;
+        *) echo "got argument $1"
+        ;;
+    esac
+    shift
+done
+
+
 if [ "${RECEIVER}" = "cc1101" ]; then
   echo "compiler.cpp.extra_flags=-DOTHER_BOARD_WITH_CC1101=1" > /usr/local/share/arduino/hardware/arduino/avr/platform.local.txt
 fi
 
+
 if [ "${BOARD}" = "nano" ]; then
   arduino --board arduino:avr:nano:cpu=atmega328 --save-prefs 2>&1
+
 elif [ "${BOARD}" = "minicul" ]; then
   echo "compiler.cpp.extra_flags=-DARDUINO_ATMEGA328P_MINICUL=1" > /usr/local/share/arduino/hardware/arduino/avr/platform.local.txt
   arduino --board arduino:avr:pro:cpu=8MHzatmega328 --save-prefs 2>&1
@@ -20,3 +38,5 @@ fi
 
 echo "Compile now for ${BOARD} with ${RECEIVER}"
 arduino -v --verbose-build --verify $PWD/RF_Receiver/RF_Receiver.ino 2>&1
+echo "Compile ${BOARD} ${RECEIVER} finshed"
+mv $PWD/release//RF_Receiver.ino.hex $PWD/release/SIGNALDuino_${BOARD}${RECEIVER}${TRAVIS_TAG}.hex
