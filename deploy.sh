47907cfcbd9ef69c42b5c40f0bda8dcbc7a66712
sidey79 <7968127+sidey79@users.noreply.github.com>
2018-03-07 00:49:56 +0100

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#set -e
+set -e
 
 #if [ "$TRAVIS_TAG" =~ "^release.*$" OR "$TRAVIS_TAG" =~ "^R\d\.\d.*$" AND "$TRAVIC_BRANCH" = "dev-r33_cc1101"  ]
 if [[ ${TRAVIS_TAG} =~ ^release.*$  || $TRAVIS_TAG =~ ^R\d\.\d.*$ && "$TRAVIC_BRANCH" = "dev-r33_cc1101" ]]
@@ -15,11 +15,12 @@ if [[ ${TRAVIS_TAG} =~ ^release.*$  || $TRAVIS_TAG =~ ^R\d\.\d.*$ && "$TRAVIC_BR
       --name "SIGNALDuino_${BOARD}${RECEIVER}${TRAVIS_TAG}"
   fi
   
-if [[ $TRAVIS_TAG =~ ^nightly.*$  ]]
-  then
+#if [[ $TRAVIS_TAG =~ ^nightly.*$  ]]
+#  then
     DATE=`date +%Y-%m-%d`
     curl --location "https://github.com/tfausak/github-release/releases/download/1.1.4/github-release-1.1.4-$TRAVIS_OS_NAME.gz" > github-release.gz
     gunzip github-release.gz
+	chmod +x ./github-release
     ./github-release upload \
       --token "$GH_API_KEY" \
       --owner Sidey79 \
@@ -27,4 +28,4 @@ if [[ $TRAVIS_TAG =~ ^nightly.*$  ]]
       --tag "$TRAVIS_TAG" \
       --file "$PWD/release/SIGNALDuino_${BOARD}${RECEIVER}${TRAVIS_TAG}.hex" \
       --name "nightly-SIGNALDuino_${BOARD}${RECEIVER}${DATE}"
-  fi
+#  fi
