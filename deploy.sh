362af72ca1f3e9ebec957b3cd01df3bbde327e72
sidey79 <7968127+sidey79@users.noreply.github.com>
2018-03-07 22:59:32 +0100

    travis

--- deploy.sh
+++ deploy.sh
@@ -14,17 +14,18 @@ if [[ ${TRAVIS_TAG} =~ ^release.*$  || $TRAVIS_TAG =~ ^R\d\.\d.*$ && "$TRAVIC_BR
       --name "SIGNALDuino_${BOARD}${RECEIVER}${TRAVIS_TAG}"
 fi
   
-if [[ $TRAVIS_TAG =~ ^nightly.*$  ]]
+if [[ $TRAVIS_TAG =~ ^nightly.*$  ]] 
   then
     DATE=`date +%Y-%m-%d`
     curl --location "https://github.com/tfausak/github-release/releases/download/1.1.4/github-release-1.1.4-$TRAVIS_OS_NAME.gz" > github-release.gz
     gunzip github-release.gz
-	chmod +x ./github-release
+    chmod +x ./github-release
     ./github-release upload \
       --token "$GH_API_KEY" \
       --owner Sidey79 \
       --repo RFD-FHEM/SIGNALDuino \
       --file "$PWD/release/SIGNALDuino_${BOARD}${RECEIVER}${TRAVIS_TAG}.hex" \
       --name "nightly-SIGNALDuino_${BOARD}${RECEIVER}${DATE}" \
-      --tag "$TRAVIS_TAG" \
+      --tag "$TRAVIS_TAG" 
 fi
+
