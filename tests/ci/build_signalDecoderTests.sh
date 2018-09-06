eabd636dd5e602353af4e37ab6b9592faea1e1da
sidey79 <7968127+sidey79@users.noreply.github.com>
2018-09-06 21:50:52 +0200

    updated build script

--- tests/ci/build_signalDecoderTests.sh
+++ tests/ci/build_signalDecoderTests.sh
@@ -10,7 +10,7 @@ fi
 export GTEST_ROOT=$TRAVIS_BUILD_DIR/third_parties/googletest/install
 export rapidassist_DIR=$TRAVIS_BUILD_DIR/third_parties/RapidAssist/install
 export win32arduino_DIR=$TRAVIS_BUILD_DIR/install
-export TEST_PROJECT_DIR=$TRAVIS_BUILD_DIR/tests/testSignalDecoder/
+export TEST_PROJECT_DIR=$TRAVIS_BUILD_DIR/tests
 
 
 
