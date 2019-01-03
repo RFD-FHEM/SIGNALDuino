f151833d8c343dd95f79d03b2634f98a19ed4d2b
sidey79 <7968127+sidey79@users.noreply.github.com>
2019-01-03 23:03:02 +0100

    travis.yml updated git get tag
    SIGNALDuino.ino
    specified as nightly version

--- SIGNALDuino.ino
+++ SIGNALDuino.ino
@@ -35,7 +35,7 @@
 #include "compile_config.h"
 
 
-#define PROGVERS               "3.3.1-RC-X-171218"
+#define PROGVERS               "3.3.1-RC-nightly"
 #define PROGNAME               " SIGNALduino "
 #define VERSION_1               0x33
 #define VERSION_2               0x1d
