3afdeb0aec52e0efcd65462ad348e49e015d6679
sidey79 <7968127+sidey79@users.noreply.github.com>
2019-01-19 23:49:28 +0100

    travis / build.sh
    different copy command for esp and arduino boards

--- build.sh
+++ build.sh
@@ -39,4 +39,3 @@ fi
 echo "Compile now for ${BOARD} with ${RECEIVER}"
 arduino -v --verbose-build --verify $PWD/SIGNALDuino/SIGNALDuino.ino 2>&1
 echo "Compile ${BOARD} ${RECEIVER} finshed"
-mv $PWD/release/SIGNALDuino.ino.hex $PWD/release/SIGNALDuino_${BOARD}${RECEIVER}${TRAVIS_TAG}.hex
