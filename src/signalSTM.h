424c532c1ec8fe3330c8c2737ac310b6fe804a6b
HomeAutoUser <abfall-container@gmx.de>
2020-06-22 11:59:48 +0200

    revised restrictions

--- src/signalSTM.h
+++ src/signalSTM.h
@@ -39,7 +39,7 @@
 #define VERSION_1               0x33
 #define VERSION_2               0x1d
 
-#if !defined(__AVR__) // MR how is right????
+#if !defined(__AVR__) && defined(MAPLE_SDUINO)
 
 #define PROGNAME               " SIGNALduino_STM "
 
