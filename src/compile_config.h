00089bb67fe52c0af4ef22ec06a9cca9e9a31baf
sidey79 <7968127+sidey79@users.noreply.github.com>
2021-05-26 22:19:11 +0200

    Update compile_config.h (#156)
    * Update signalduino.h
    
    Add all pins as input pullup

--- src/compile_config.h
+++ src/compile_config.h
@@ -44,7 +44,8 @@
  * ****************************************
 */
 
-#define PROGVERS               "3.5.0-dev+20201221"
+
+#define PROGVERS               "3.5.0-dev+20210420"
 
 #ifdef OTHER_BOARD_WITH_CC1101
   #define CMP_CC1101
--- src/signalduino.h
+++ src/signalduino.h
@@ -96,8 +96,9 @@ void setup() {
     ; // wait for serial port to connect. Needed for native USB
   }
 
-
-
+  for (uint8_t i=2;i<13;i++) {
+    pinAsInputPullUp(i);
+  }
   //delay(2000);
   pinAsInput(PIN_RECEIVE);
   pinAsOutput(PIN_LED);
