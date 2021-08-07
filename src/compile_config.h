782668701955837fe46f54102b744ffc716e9d1e
sidey79 <7968127+sidey79@users.noreply.github.com>
2021-08-08 00:35:04 +0200

    compile_config.h
    update version to todays date

--- src/compile_config.h
+++ src/compile_config.h
@@ -44,7 +44,7 @@
  * ****************************************
 */
 
-#define PROGVERS               "3.5.0-dev+20201221"
+#define PROGVERS               "3.5.0-dev+20210808"
 
 #ifdef OTHER_BOARD_WITH_CC1101
   #define CMP_CC1101
