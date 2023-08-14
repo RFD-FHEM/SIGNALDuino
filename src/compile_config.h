75228a783d0325463f27d208a9390ec9bff37612
sidey79 <7968127+sidey79@users.noreply.github.com>
2023-08-14 23:54:32 +0200

    chore!: Additional FREQEST output - needs module with FREQEST support

--- src/compile_config.h
+++ src/compile_config.h
@@ -45,7 +45,7 @@
 */
 
 #ifndef PROGVERS
-  #define PROGVERS               "3.5.2+20230417"   // platformio will set this to the correct value (lateste tag with commits since latest tag)
+  #define PROGVERS               "4.0.0+20230814"   // platformio will set this to the correct value (lateste tag with commits since latest tag)
 #endif
 
 #ifdef OTHER_BOARD_WITH_CC1101
