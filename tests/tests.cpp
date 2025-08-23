b86ee26b058bbbc75617747c33b437157232c805
Udo <info@elektron-bbs.de>
2025-08-23 11:25:46 +0200

    Update tests.cpp

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -109,7 +109,7 @@ namespace arduino {
 					buckets[counter] = atoi(cmdstring->substr(startpos + 3, endpos).c_str());
 					if (raw_mode)
 					{
-						ooDecode.pattern[counter] = buckets[counter];
+						ooDecode.addpattern[counter] = buckets[counter];
 					}
 
 				}
